a0029ff0341eafbedaa9249b2428eb3643ad4212
Donaldo-1997 <donaldojbm7@gmail.com>
2022-09-01 18:26:45 -0500

    refactor navbar

--- client/src/components/NavBar/Navbar.jsx
+++ client/src/components/NavBar/Navbar.jsx
@@ -1,28 +1,58 @@
 import React from 'react'
-import LoginButtonAuth0 from "../authzero/LoginButtonAuth0";
-import LogoutButtonAuth0 from "../authzero/LogoutButtonAuth0";
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { FaHeart, FaUserAlt } from "react-icons/fa";
+import { BsFillCartFill } from "react-icons/bs";
+import { BiLogOut } from "react-icons/bi";
+import { Link } from 'react-router-dom'
 import { useAuth0 } from "@auth0/auth0-react";
-import styles from "./Navbar.module.css";
+import LogoutButtonAuth0 from "../authzero/LogoutButtonAuth0";
+import LoginButtonAuth0 from "../authzero/LoginButtonAuth0";
+import Searchbar from "../SearchBar/SearchBar";
+import styles from "./NavBar.module.css";
+import User from '../authzero/User';
 
-function Navbar() {
-    const { isAuthenticated } = useAuth0()
+function NavBar() {
+
   return (
-    <div>
-        {isAuthenticated ? (
-              <div className={styles.containerLogout}>
-                <div>
-                  <LogoutButtonAuth0 
-                    className={styles.logout_button}
-                  />
-                </div>
-              </div>
-            ) : (
-              <div className={styles.containerLogout}>
-                <LoginButtonAuth0 className={styles.icon}/>
-              </div>
-            )}
-    </div>
+    <Navbar expand='lg' className={styles.navbar}>
+      <Container fluid>
+        <button onClick={(e) => console.log('handleReset(e)')} className={styles.resetButton}>
+          <Navbar.Brand className={styles.yourShoes}>
+            Your<span>Shoes</span>
+          </Navbar.Brand>
+        </button>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <div className={styles.containerLogout}>
+              <Nav.Link className={styles.icon}>
+                <Link to="/cart" className={styles.Link}>
+                  {" "}
+                  <BsFillCartFill style={{ color: "#f87d2d" }} />
+                </Link>
+              </Nav.Link>
+            </div>
+            <div className={styles.containerLogout}>
+              <Nav.Link className={styles.icon}>
+                <Link to="/favorites">
+                  <FaHeart style={{ color: "#f87d2d" }} />
+                </Link>
+              </Nav.Link>
+            </div>
+          </Nav>
+          <Searchbar
+            handleInputName={() => console.log('handleInputName')}
+            handleNameSubmit={() => console.log('handleNameSubmit')}
+          />
+          <User />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
-export default Navbar
+export default NavBar
