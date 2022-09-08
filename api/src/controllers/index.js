978ed896030d5aded0d3b72d997915c5d945b792
Donaldo-1997 <donaldojbm7@gmail.com>
2022-09-08 10:28:23 -0500

    Arreglado problema de llave duplicada en products

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,6 +1,6 @@
 const { Product, Category, Brand, Size } = require("../db");
 const axios = require("axios");
-const {Op} = require ("sequelize");
+const { Op } = require("sequelize");
 
 const setDataApi = async () => {
   const url = "https://api.mercadolibre.com/sites/MLA/search?category=";
@@ -16,23 +16,24 @@ const setDataApi = async () => {
     "MLA414674&BRAND=1088662",
     "MLA414610&BRAND=2658635",
     "MLA415192&BRAND=1088662",
-    "MLA414673&BRAND=238731",
-    // 'MLA455893', 'MLA414673' //---> NO TIENEN BRAND
   ].map((e) => url + e); // armo la url ej: "https://api.mercadolibre.com/sites/MLA/search?category=MLA109027&BRAND=14671"
- 
+
   const getAllApi = await Promise.all(
     ids.map(async (link) => {
       return (await axios(link)).data.results;
     })
-  );
+  )
+
   return getAllApi
 }
-const cargoalDB =async()=>{ 
-  
-  const getAllApi1  =await setDataApi()
-  const data =getAllApi1.flat().map((e) => {
-    e.size = [{id:1, number: 35, stock:2, solds:0}, {id:2, number:36, stock:2, solds:0},{id:3, number:37, stock:2, solds:0},{id:4, number:38, stock:1, solds:0},{id: 5, number:39, stock:1, solds:0},{id:6, number:40, stock:1, solds:0},{id:7, number:41, stock:1, solds:0},{id:8, number:42, stock:1, solds:0},{id:9, number:43, stock:1, solds:0}]
-   
+
+const cargoalDB = async () => {
+
+  const getAllApi1 = await setDataApi()
+
+  const data = getAllApi1.flat().map((e) => {
+    e.size = [{ id: 1, number: 35, stock: 2, solds: 0 }, { id: 2, number: 36, stock: 2, solds: 0 }, { id: 3, number: 37, stock: 2, solds: 0 }, { id: 4, number: 38, stock: 1, solds: 0 }, { id: 5, number: 39, stock: 1, solds: 0 }, { id: 6, number: 40, stock: 1, solds: 0 }, { id: 7, number: 41, stock: 1, solds: 0 }, { id: 8, number: 42, stock: 1, solds: 0 }, { id: 9, number: 43, stock: 1, solds: 0 }]
+
     return ({
       id: e.id,
       title: e.title,
@@ -41,51 +42,61 @@ const cargoalDB =async()=>{
       model: e.attributes && e.attributes.length === 3 ? e.attributes[2].value_name : "Not found",
       price: e.price, //parseInt(s.price)
       category: e.category_id,
-      size: e.size ? e.size.map(s=>{
-        return({
+      size: e.size ? e.size.map(s => {
+        return ({
           id: s.id,
           number: s.number,
           stock: s.stock,
           solds: s.solds,
-          isActive: s.stock===0?false:true
+          isActive: s.stock === 0 ? false : true
         })
       }) : "Not found"
     });
   })
- 
-  // const shoesConflicts = ['MLA1142122158', 'MLA1120923058', 'MLA1122533893']
-  const shoesConflicts = [
-    'Zapatillas Downshifter 11 Nike Nike Tienda Oficial',
-    'Zapatillas W React Element 55 Nike Blast Tienda Oficial',
-    'Zapatillas Nike Venture Runner Neg De Hombre'
-  ]
+
+  const shoesConflicts = ['MLA1142122158', 'MLA1120923058', 'MLA1122533893', 'MLA1148732023']
   const cargoFinal = data.filter(e => !shoesConflicts.includes(e.id))
 
   return cargoFinal
 }
-  //cargo los productos al db y necesita que ya este cargada las categoria para que se cree la relacion
- const getAllProducts = async()=>{
+//cargo los productos al db y necesita que ya este cargada las categoria para que se cree la relacion
+const getAllProducts = async () => {
   const cargoFinal1 = await cargoalDB()
-   await Promise.all(
-     cargoFinal1.flat().map(async (el) => {
-       const newProduct = await Product.create(el);
-       const foundBrand = await Brand.findByPk(el.brand);
-       const foundSize = el.size.map(s=>s.id)
-       const foundCategories = await Category.findByPk(el.category);
-       await newProduct.addBrand(foundBrand);
-       await newProduct.addSizes(foundSize);
-       await newProduct.addCategory(foundCategories);
-       return newProduct;
-     })
-   );
-   let dataDb = await Product.findAll({
-    where:{isActive:true},
-        include:  [
-    { model: Size, where:{isActive:true}},
-    { model: Brand },
-    { model: Category },
-  ] });
-   return dataDb;
+  await Promise.all(
+    cargoFinal1.flat().map(async (el) => {
+
+      const [ newProduct, created ] = await Product.findOrCreate({ 
+        where: { id: el.id }, 
+        defaults: {
+          title: el.title,
+          image: el.image,
+          model: el.model,
+          price: el.price
+        } 
+      });
+      
+      if (!created) {
+        const foundBrand = await Brand.findByPk(el.brand);
+        const foundSize = el.size.map(s => s.id)
+        const foundCategories = await Category.findByPk(el.category);
+        await newProduct.addBrand(foundBrand);
+        await newProduct.addSizes(foundSize);
+        await newProduct.addCategory(foundCategories);
+        return newProduct;
+      }
+    })
+  );
+
+  const dataDb = await Product.findAll({
+    where: { isActive: true },
+    include: [
+      { model: Size, where: { isActive: true } },
+      { model: Brand },
+      { model: Category },
+    ]
+  });
+
+  return dataDb;
 }
 
 const getDbCategories = async () => {
@@ -95,10 +106,13 @@ const getDbCategories = async () => {
 const getDbBrand = async () => {
   try {
     const brands = await Brand.findAll({
-      where:{name:{
-        [Op.and]:[{[Op.notILike]:`%sport%`},{[Op.notILike]:`%pampa%`}]}}
+      where: {
+        name: {
+          [Op.and]: [{ [Op.notILike]: `%sport%` }, { [Op.notILike]: `%pampa%` }]
+        }
+      }
     });
-    
+
     return brands;
 
   } catch (error) {
@@ -106,7 +120,7 @@ const getDbBrand = async () => {
   }
 }
 const getDbSize = async () => {
-  const foundSize = await Size.findAll({ order:[['number',"ASC"]], include: { all: true } });
+  const foundSize = await Size.findAll({ order: [['number', "ASC"]], include: { all: true } });
   return foundSize;
 };
 
