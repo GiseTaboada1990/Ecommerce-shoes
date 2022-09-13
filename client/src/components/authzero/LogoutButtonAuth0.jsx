e762898046dd44cb6ed681401d2b4029aaf0f239
GiseTaboada1990 <giseltaboada1990@gmail.com>
2022-09-13 18:05:20 -0300

    variable deploy vercel

--- client/src/components/authzero/LogoutButtonAuth0.jsx
+++ client/src/components/authzero/LogoutButtonAuth0.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { BiLogOut } from "react-icons/bi";
 import { URL } from '../../redux/actions';
+export const DEPLOY_URL = process.env.REACT_APP_DEPLOY_URL
 
 export default function LogoutButtonAuth0() {
     const { logout } = useAuth0()
@@ -16,7 +17,7 @@ export default function LogoutButtonAuth0() {
     
     return (
         <a
-            href={`${URL}/`}
+            href={`${DEPLOY_URL}/`}
             onClick={() =>handleLogout ()}
         >
             <BiLogOut style={{ color: "#f87d2d" }}></BiLogOut>
